7fe0d200bfdbb7cc339132c103fe0baea9731d12
jennyjean <jennyjean8675309@yahoo.com>
2019-04-26 13:41:26 -0400

    added plusFiveOrTimesThree

---
+++ plusFiveOrTimesThree.js
@@ -0,0 +1,19 @@
+//Consider this puzzle: by starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite set of numbers can be produced. How would you write a function that, given a number, tries to find a sequence of such additions and multiplications that produces that number?
+
+//Example: the number 13 could be reached by first multiplying by 3 and then adding 5 twice, whereas the number 15 cannot be reached at all.
+
+//Recursive_solution:
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+             find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
